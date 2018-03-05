329dce473b8ec7b9f4a7e12957e706d1bb9e56de
Stephen Tilley <stephentilley@gmail.com>
2018-03-05 17:13:09 +0000

    Added specrunner file to run our tests

---
+++ spec/itSpec.js
@@ -0,0 +1,7 @@
+// var undefinedVar
+//
+// it("has property toEqual").expect(it.toEqual).not.toEqual(undefinedVar)
+
+it("1 equals 1").expect(1).toEqual(1)
+
+it("[1, 2, 3] contains 2").expect([1,2,3]).toContain(2)
