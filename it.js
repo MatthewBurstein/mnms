ce4a3fdd56628f165776207b8861563d5f0c72d9
Noel Vock <noel1uk@gmail.com>
2018-03-06 01:08:22 +0000

    Add describe function.

--- it.js
+++ it.js
@@ -1,5 +1,5 @@
 var it = function(description) {
-  this.description = description
+  this.description = description;
 
   this.expect = function(expectedValue) {
     this.expected = expectedValue;
@@ -23,9 +23,17 @@ var it = function(description) {
   };
 
   this._displayDescription = function() {
-    console.log(description);
+    console.log('It', description);
     console.log('Expected: ', this.expected, "\n  Got: ", this.actual)
   };
 
-  return this;
+// Remove this
+  // function test() {
+  //     document.getElementById('content').innerHTML = '<p>' + this.expected + '</p>';
+  //
+  // }
+  // window.onload = test;
+//
+return this;
+
 };
--- spec/itSpec.js
+++ spec/itSpec.js
@@ -1,7 +1,6 @@
 // var undefinedVar
 //
 // it("has property toEqual").expect(it.toEqual).not.toEqual(undefinedVar)
+describe(".toEqual", it("checks 1 equals 1").expect(1).toEqual(1));
 
-it("1 equals 1").expect(1).toEqual(1)
-
-it("[1, 2, 3] contains 2").expect([1,2,3]).toContain(2)
+describe(".toContain", it("[1, 2, 3] contains 2").expect([1,2,3]).toContain(2));
