2749a12919235b5a1ceea5dc75e515b6d46043c2
Noel Vock <noel1uk@gmail.com>
2018-03-07 15:55:23 +0000

    Test results appear on mnmsSpecRunner.html as html.

--- spec/mnms/it.js
+++ spec/mnms/it.js
@@ -3,8 +3,19 @@ const beforeEach = function(callback) {
   beforeEachCallback = callback;
 }
 
+var output = function(description) {
+  var node = document.createElement("P");
+  var textnode = document.createTextNode(description);
+  console.log(textnode);
+  node.appendChild(textnode);
+  document.getElementById("content").appendChild(node);
+};
+
+
+
 var it = function(description, expectAndMatcher) {
   console.log('\tIt', description);
+  output('It ' + description);
   if (beforeEachCallback !== undefined) {
     beforeEachCallback()
   }
@@ -44,14 +55,29 @@ var expect = function(expectedValue) {
 
   this._displayExpectedActual = function() {
     console.log('\t\tExpected: ', this.expected, "\n\t\tGot: ", this.actual);
+    console.log(this.expected);
+    var expected = this.expected;
+    var actual = this.actual;
+    var outputExpected = 'Expected: ' + expected;
+    var outputActual = 'Got: ' + actual
+    output(outputExpected);
+    output(outputActual);
   };
 
   this._displaySuccessMessage = function(successString) {
     console.log("%c\t\tTest passed. ", "color: green; background-color: #c5ffb2;", this.expected, successString, this.actual)
+    var expected = this.expected;
+    var actual = this.actual;
+    var successMessage= 'Test passed ' + expected + successString + actual;
+    output(successMessage);
   }
 
   this._displayErrorMessage = function(errorString) {
     console.error("\t\tTest failed. ", this.expected, errorString, this.actual);
+    var expected = this.expected;
+    var actual = this.actual;
+    var errorMessage= 'Test passed ' + expected + errorString + actual;
+    output(errorMessage);
   };
 
   return this;
