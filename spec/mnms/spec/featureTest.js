a6ad5a05bf1dfa930b8632523272eaf5d41932c5
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-07 18:53:05 +0000

    Pass click button feature test

--- spec/mnms/spec/featureTest.js
+++ spec/mnms/spec/featureTest.js
@@ -1,6 +1,8 @@
-describe("click a button", function() {
-  it("changes the content when I call click button", function() {
-    clickButton('myButton');
-    expect("class1").tohavecontent("buttonHasBeenClicked")
+document.addEventListener("DOMContentLoaded", function(event) {
+  describe("click a button", function() {
+    it("changes the content when I call click button", function() {
+      clickButtonById('test-button');
+      expect(htmlSelectorId("test-button-container")).toEqual("buttonHasBeenClicked")
+    });
   });
 });
